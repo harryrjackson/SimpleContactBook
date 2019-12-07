88cab2202538086ae6902acc14cf001317e45a76
HJ <harold.ray.jackson@gmail.com>
2019-12-07 15:55:52 -0600

    Added RelayCommand.

---
+++ SimpleContactBook/SimpleContactBook/Utility/RelayCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SimpleContactBook.Utility
+{
+    public class RelayCommand<T> : ICommand
+    {
+
+        #region Variables
+
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        #endregion
+
+        #region Constructors
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute) 
+        {
+            if (execute == null)
+            {
+                throw new NullReferenceException(nameof(execute));
+            }
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+
+        }
+
+        #endregion
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        #region Properties
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+
+        #endregion
+    }
+}
