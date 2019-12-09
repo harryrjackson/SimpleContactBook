7db72f53f901d2489cb3863d10bd68c5965e4f71
HJ <harold.ray.jackson@gmail.com>
2019-12-08 18:17:33 -0600

    Added Converters

--- SimpleContactBook/SimpleContactBook/ViewModels/ContactsViewModel.cs
+++ SimpleContactBook/SimpleContactBook/ViewModels/ContactsViewModel.cs
@@ -3,6 +3,8 @@ using SimpleContactBook.Utility;
 using SimpleContactBook.Models;
 using System.Collections.ObjectModel;
 using System.Collections.Generic;
+using System.Windows.Input;
+using System;
 
 namespace SimpleContactBook.ViewModels
 {
@@ -16,11 +18,43 @@ namespace SimpleContactBook.ViewModels
             set { OnPropertyChanged(ref _selectedContact,  value); }
         }
 
+        private bool _isEditMode;
+        public bool IsEditMode
+        {
+            get { return _isEditMode; }
+            set
+            {
+                OnPropertyChanged(ref _isEditMode, value);
+                OnPropertyChanged(nameof(IsDisplayMode));
+            }
+        }
+
+        public bool IsDisplayMode
+        {
+            get { return !_isEditMode; }
+        }
+
         public ObservableCollection<Contact> Contacts { get; private set; }
+
+        public ICommand EditCommand { get; private set; }
 
         public ContactsViewModel()
         {
+            EditCommand = new RelayCommand(Edit, CanEdit);
+        }
 
+        private bool CanEdit()
+        {
+            if (SelectedContact == null)
+            {
+                return false;
+            }
+            return !IsEditMode;
+        }
+
+        private void Edit()
+        {
+            IsEditMode = true;
         }
 
         public void LoadContacts(IEnumerable<Contact> contacts)
